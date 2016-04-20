3ac38f01a42a87552852dc57a3c2645968528257
Fredrik Holmar <fredrik1@iMac27.local>
2016-04-20 21:52:04 +0200

    A bunch of auth stuff like the user's top artists, and the ability to save lists.

---
+++ playlists/auth.js
@@ -0,0 +1,110 @@
+
+// grab the GET parameters in case we have an auth code or state
+
+old_hash = "#"
+HASH_PARAMS = []
+
+token = null
+valid_until = null
+
+user_id = null
+user_country = null
+
+function token_valid() {
+
+    return token != null
+
+}
+
+function add_token_header(request) {
+
+    if (token_valid()) {
+
+        request.setRequestHeader("Authorization","Bearer "+token)
+
+    }
+
+}
+
+function make_parameters(s) {
+
+    if (s.length == 0) {
+        return
+    }
+
+    s.substring(1).split('&').forEach(function(a){
+
+        b = a.split("=")
+        HASH_PARAMS[b[0]] = b[1]
+
+    })
+
+}
+
+function load_token() {
+
+    if (typeof HASH_PARAMS['access_token'] != "undefined") {
+
+        token = HASH_PARAMS['access_token']
+
+    }
+
+}
+
+function do_sign_in() {
+
+    base_url = "http://fx21.github.io/playlists/"
+    scope = "user-read-private user-top-read user-follow-read playlist-modify-public playlist-modify-private"
+
+    //<a href='https://accounts.spotify.com/authorize/?client_id=6c687a3e2125432db6b91368aa48cad0&response_type=token&redirect_uri=http%3A%2F%2Ffx21.github.io%2Fplaylists%2F&scope=user-read-private%20user-top-read&state=34fFs29kd09' target='_blank'>Sign in</a>
+
+    url = "https://accounts.spotify.com/authorize/?client_id=6c687a3e2125432db6b91368aa48cad0&response_type=token&redirect_uri="+encodeURIComponent(base_url)+"&scope="+encodeURIComponent(scope)+"&state=34fFs29kd09"
+
+    window.location.replace(url)
+
+}
+
+function validate_token_callback(response) {
+
+    user_id = response["id"]
+    user_country = response["country"]
+
+    // this needs to be exposed graphically somehow
+    use_market = user_country
+
+    $("#signinbtn").addClass("btn-success").prop("disabled",true).text('Signed in as '+user_id)
+
+}
+
+function validate_token_error(response) {
+
+    token = null
+
+}
+
+function validate_token() {
+
+    // try to grab the current user name and find out if the token is valid in the process
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        beforeSend: add_token_header,
+        data: {
+        },
+        success: validate_token_callback,
+        error: validate_token_error
+    });
+
+
+}
+
+make_parameters(location.hash)
+load_token()
+
+// clear data for now
+old_hash = location.hash
+window.location.replace("#")
+
+validate_token()
+
+
--- playlists/playlist.js
+++ playlists/playlist.js
@@ -1,9 +1,203 @@
 
+playlist_source_name = "nothing"
+playlist_fuzziness = 2
+
+function make_new_playlist_name() {
+
+    // try to express the fuzziness as a text to use as the playlist name
+
+    fuzzy_flavor = ["random tracks from"]
+
+    if (playlist_fuzziness == 1) {
+        fuzzy_flavor = ["tracks from {}",
+            "tried and true from {}",
+            "nothing but {}",
+            "{} playlist",
+            "{} list"]
+    } else if (playlist_fuzziness == 2) {
+        fuzzy_flavor = ["tracks related to {}",
+            "tracks from and related to {}",
+            "{} and related",
+            "{} and more",
+            "{} etc"]
+    } else if (playlist_fuzziness <= 5) {
+        fuzzy_flavor = ["random tracks related to {}",
+            "rando tracks for {}",
+            "adventurous list based on {}",
+            "fuzzy list: {}"]
+    } else if (playlist_fuzziness <= 10) {
+        fuzzy_flavor = ["very random tracks related to {}",
+            "crazy list based on {}"]
+    } else if (playlist_fuzziness <= 20) {
+        fuzzy_flavor = ["very VERY random tracks related to {}",
+            "{}: i don't even know edition"]
+    } else {
+        fuzzy_flavor = ["super random tracks having almost no relation to {}",
+            "a musical adventure featuring {}",
+            "a random walk from {}",
+            "{}: a random walk",
+            "{} and lots of others"]
+    }
+
+    len = fuzzy_flavor.length
+    use_flavor = fuzzy_flavor[Math.floor(len*Math.random())]
+
+    smile = [":o)",":)",":D","=^_^="]
+    use_smile = smile[Math.floor(smile.length*Math.random())]
+
+    $("#plname").val(use_flavor.replace("{}",playlist_source_name) + " " + use_smile)
+
+}
+
+function update_source_name(new_name) {
+
+    playlist_source_name = new_name
+
+    make_new_playlist_name()
+}
+
+function update_fuzziness() {
+
+    playlist_fuzziness = parseInt($("#plfuzz").val())
+
+    make_new_playlist_name()
+}
+
+function update_seed_source() {
+
+    artists = $("#seedarea").val().split("\n").filter(function(s){return s})
+
+    value = "nothing"
+
+    if (artists.length == 1) {
+        value = artists[0]
+    } else if (artists.length == 2) {
+        value = artists[0] + " and " + artists[1]
+    } else if (artists.length == 3) {
+        value = artists[0] + ", " + artists[1] + ", and " + artists[2]
+    } else if (artists.length > 20) {
+        value = "a whole bunch of artists"
+    } else {
+        value = artists[0] + ", " + artists[1] + ", " + artists[2] + ", and others"
+    }
+
+    update_source_name(value)
+
+}
+
+function clear_list() {
+
+    $("#seedarea").val('')
+
+    update_source_name("nothing")
+
+}
+
+function add_to_seed(string) {
+
+    prev_val = $("#seedarea").val()
+
+    // in case the text area doesn't end in a newline, add it
+
+    if (prev_val.length > 0 && !prev_val.endsWith('\n')) {
+        $("#seedarea").val($("#seedarea").val()+"\n");
+    }
+
+    $("#seedarea").val($("#seedarea").val()+string+"\n");
+
+}
+
+function user_top_callback(response) {
+
+    response["items"].forEach(function(a) {
+
+        add_to_seed(a["name"]);
+
+    })
+
+}
+
+function user_top_artists(length) {
+
+    if (token_valid()) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/top/artists',
+            beforeSend: add_token_header,
+            data: {
+                time_range: length,
+                limit: 50
+            },
+            success: user_top_callback
+        });
+
+        if (length == 'short_term') {
+            update_source_name("my recent top artists")
+        } else if (length == 'long_term') {
+            update_source_name("my all time top artists")
+        } else {
+            update_source_name("my top artists")
+        }
+
+
+    }
+
+}
+
+function user_followed_callback(response) {
+
+    response["artists"]["items"].forEach(function(a) {
+
+        add_to_seed(a["name"]);
+
+    })
+
+    // check if there's more
+
+    cursor_after = response["artists"]["cursors"]["after"]
+
+    if (cursor_after) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/following',
+            beforeSend: add_token_header,
+            data: {
+                type: 'artist',
+                limit: 50,
+                after: cursor_after
+            },
+            success: user_followed_callback
+        });
+
+    }
+
+}
+
+function user_followed_artists() {
+
+    if (token_valid()) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/me/following',
+            beforeSend: add_token_header,
+            data: {
+                type: 'artist',
+                limit: 50
+            },
+            success: user_followed_callback
+        });
+
+        update_source_name("my followed artists")
+
+    }
+
+}
+
 function related_artists_callback(response) {
 
     response["artists"].forEach(function(a) {
 
-        $("#seedarea").val($("#seedarea").val()+a["name"]+"\n");
+        add_to_seed(a["name"]);
 
     })
 
@@ -13,11 +207,12 @@ function search_artist_callback(response) {
 
     a = response["artists"]["items"][0]
 
-    $("#seedarea").val($("#seedarea").val()+a["name"]+"\n");
+    add_to_seed(a["name"]);
     id = a["id"]
 
     $.ajax({
         url: 'https://api.spotify.com/v1/artists/'+id+'/related-artists',
+        beforeSend: add_token_header,
         data: {
         },
         success: related_artists_callback
@@ -28,6 +223,7 @@ function search_artist_callback(response) {
 function searchArtist(query) {
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
+        beforeSend: add_token_header,
         data: {
             q: query,
             type: 'artist'
@@ -40,13 +236,15 @@ function related_search() {
 
     searchArtist($("#relartists").val())
 
+    update_source_name($("#relartists").val())
+
 }
 
 function search_genre_callback(response) {
 
     response["artists"]["items"].forEach(function(a) {
 
-        $("#seedarea").val($("#seedarea").val()+a["name"]+"\n");
+        add_to_seed(a["name"]);
 
     })
 
@@ -55,6 +253,7 @@ function search_genre_callback(response) {
 function searchGenre(query) {
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
+        beforeSend: add_token_header,
         data: {
             q: query,
             type: 'artist'
@@ -67,11 +266,15 @@ function genre_search() {
 
     searchGenre("genre:"+$("#genresearch").val())
 
+    update_source_name($("#genresearch").val())
+
 }
 
 function years_search() {
 
     searchGenre("year:"+$("#yearssearch").val())
+
+    update_source_name("years "+$("#yearssearch").val())
 
 }
 
@@ -79,9 +282,11 @@ function editor() {
 
     editors_picks.forEach(function(a){
 
-        $("#seedarea").val($("#seedarea").val()+a+"\n");
+        add_to_seed(a);
 
     })
+
+    update_source_name("dj meow mix top 500")
 
 }
 
@@ -94,6 +299,8 @@ function pllen(value) {
 function fuzz(value) {
 
     $("#plfuzz").val(value)
+
+    update_fuzziness()
 
 }
 
@@ -177,6 +384,7 @@ function jump_to_related() {
 
     $.ajax({
         url: 'https://api.spotify.com/v1/artists/'+id+'/related-artists',
+        beforeSend: add_token_header,
         data: {
         },
         success: chain_related_callback,
@@ -261,6 +469,7 @@ function chain_album_callback(response) {
 
     $.ajax({
         url: 'https://api.spotify.com/v1/albums/'+id+'/tracks',
+        beforeSend: add_token_header,
         data: {
             market: use_market
         },
@@ -280,6 +489,7 @@ function chain_artist(a) {
 
     $.ajax({
         url: 'https://api.spotify.com/v1/artists/'+id+'/albums',
+        beforeSend: add_token_header,
         data: {
             type: "album",
             market: use_market
@@ -314,6 +524,7 @@ function start_chain() {
 
     $.ajax({
         url: 'https://api.spotify.com/v1/search',
+        beforeSend: add_token_header,
         data: {
             q: starting_point,
             type: 'artist'
@@ -369,6 +580,103 @@ function make_it() {
 function rawdata() {
 
     $("#rawarea").toggleClass("hidden")
+
+}
+
+current_playlist_save_offset = 0
+current_playlist_save_id = null
+
+function chunk_save_error(response) {
+
+    $("#savebtn").addClass("btn-danger").text("Save failed at track "+(current_playlist_save_offset-100))
+
+}
+
+function save_next_chunk() {
+
+    if (current_playlist_save_offset >= generated_list.length) {
+
+        //we're past the end of the list, time to stop saving
+        $("#savebtn").addClass("btn-success").text("Saved")
+        return
+
+    }
+
+    databody = '{"uris":['
+
+    for (i = 0; i < 100; i++) {
+
+        index = current_playlist_save_offset+i
+
+        if (index < generated_list.length) {
+            databody += '"'+generated_list[index]+'"'
+        }
+
+        //if we're not at the last element, add a comma
+
+        if (i < 99 && index < generated_list.length-1) {
+            databody += ','
+        }
+
+    }
+
+    databody += ']}'
+
+    //databody = '{"uris":["spotify:track:1i766mVakKAs6VD0tf9deg"]}'
+
+    current_playlist_save_offset += 100
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/users/'+user_id+'/playlists/'+current_playlist_save_id+'/tracks',
+        method: "POST",
+        beforeSend: add_token_header,
+        data: databody,
+        success: save_next_chunk,
+        error: chunk_save_error
+    });
+
+}
+
+function save_playlist_callback(response) {
+
+    //playlist created, push tracks to it
+
+    current_playlist_save_id = response["id"]
+    save_next_chunk()
+
+}
+
+function save_playlist_error(response) {
+
+    $("#savebtn").addClass("btn-danger").text("Save failed")
+
+}
+
+function save_playlist() {
+
+    use_playlist_name = $("#plname").val().split('"').join('\\"')
+
+    if (token_valid()) {
+
+        $.ajax({
+            url: 'https://api.spotify.com/v1/users/'+user_id+'/playlists',
+            method: "POST",
+            beforeSend: add_token_header,
+            data: '{"name":"'+use_playlist_name+'","public":true}',
+            success: save_playlist_callback,
+            error: save_playlist_error
+        });
+
+        $("#savebtn").prop("disabled",true)
+
+    }
+
+}
+
+function start_over() {
+
+    window.location.replace(old_hash)
+    window.location.reload(true)
 
 }
 
